1575591f319e4e7200c88da56a4e53e89158ec60
shivamani39 <150172113+shivamani39@users.noreply.github.com>
2025-09-21 23:14:39 +0530

    Update axios.js

--- frontend/src/api/axios.js
+++ frontend/src/api/axios.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const baseURL = import.meta.env.VITE_API_URL || 'http://localhost:4000';
+const baseURL = import.meta.env.VITE_API_URL || 'https://truegradient-assignment-backend.onrender.com';
 
 const api = axios.create({ baseURL });
 
